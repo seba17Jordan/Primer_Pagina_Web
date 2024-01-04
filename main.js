ad3dfcd588ffd9511983a816b5dc5b186fd3cec6
Sebastian <sj285318@fi365.ort.edu.uy>
2024-01-04 10:32:07 -0300

    efectos desplazamento finalizados

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
+function isElementInViewport(el) { //Me dice si elemento se esta viendo
+    var rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
 function fadeInOnScroll() { //Función para efecto fade al hacer scroll
     var elementsToShow = document.querySelectorAll('.imagen-fade'); //se aplica a imagenes con esta clase
-
-    function isElementInViewport(el) { //Función que verifica si elemento está en viewport
-        var rect = el.getBoundingClientRect();
-        return (
-            rect.top >= 0 &&
-            rect.left >= 0 &&
-            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-        );
-    }
 
     function handleScroll() {//Función para manejar el scroll y mostrar las imágenes
         elementsToShow.forEach(function (element) {
@@ -25,28 +25,36 @@ function fadeInOnScroll() { //Función para efecto fade al hacer scroll
     handleScroll();//Inicializa el efecto fade al cargar la página
 }
 
-
-function fadeInSlideOnScroll() { //Función para efecto fade con desplazamiento al hacer scroll
-    var elementsToShow = document.querySelectorAll('.imagen-slide-fade');
-
-    function isElementInViewport(el) {
-        var rect = el.getBoundingClientRect();
-        return (
-            rect.top >= 0 &&
-            rect.left >= 0 &&
-            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-        );
-    }
+function fadeInSlideOnScrollRight() { //Función para efecto fade con desplazamiento a la derecha
+    var elementsToShow = document.querySelectorAll('.imagen-slide-fade-right');
 
     function handleScroll() {
         elementsToShow.forEach(function (element) {
             if (isElementInViewport(element)) {
                 element.style.opacity = "1";
-                element.style.transform = "translateX(300%)";
+                element.style.transform = "translateX(75%)";
             } else {
                 element.style.opacity = "0";
-                element.style.transform = "translateX(0)";
+                element.style.transform = "translateX(150%)";
+            }
+        });
+    }
+
+    window.addEventListener('scroll', handleScroll);
+    handleScroll();
+}
+
+function fadeInSlideOnScrollLeft() { //Función para efecto fade con desplazamiento a la derecha
+    var elementsToShow = document.querySelectorAll('.imagen-slide-fade-left');
+
+    function handleScroll() {
+        elementsToShow.forEach(function (element) {
+            if (isElementInViewport(element)) {
+                element.style.opacity = "1";
+                element.style.transform = "translateX(75%)";
+            } else {
+                element.style.opacity = "0";
+                element.style.transform = "translateX(0%)";
             }
         });
     }
@@ -56,8 +64,8 @@ function fadeInSlideOnScroll() { //Función para efecto fade con desplazamiento 
 }
 
 
-
 document.addEventListener("DOMContentLoaded", function() {
     fadeInOnScroll();//Llama funcion de fade sroll cuando contenido de la página está cargado
-    fadeInSlideOnScroll();
+    fadeInSlideOnScrollRight();
+    fadeInSlideOnScrollLeft();
 });
